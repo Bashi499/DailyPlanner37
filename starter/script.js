b2c64bd4b7973d45eec8cc5c300e931645fdc9d7
Bashi <bashiabduwali3@hotmail.com>
2023-04-05 19:38:21 +0100

    attempt 1

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,7 @@
-$('currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'))
-
-
-
+//using momemnt js to get current date
+let NowMoment = moment().format("DD.MM.YYYY HH:mm");
+let displayDate = document.getElementById("currentDay");
+displayDate.innerHTML = NowMoment;
+let currentHour = moment().format("HH");
 
 
